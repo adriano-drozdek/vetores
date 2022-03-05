b411cda3df7d35be2d2f1ae809079a4d15a27a5e
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-05 19:21:34 -0300

    Adicionando Constructor using Fields.

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -1,5 +1,19 @@
 package entities;
 
 public class Produto {
+	
+	private String nome;
+	private double preco;
+	
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public Produto() {
+		super();
+	}
+	
+	
 
 }
