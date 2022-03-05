70824f69420e4ac30a24deb7e6a3aaf653bc7af3
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-05 19:23:22 -0300

    Adicionando Getters and setters.

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -1,10 +1,10 @@
 package entities;
 
 public class Produto {
-	
+
 	private String nome;
 	private double preco;
-	
+
 	public Produto(String nome, double preco) {
 		this.nome = nome;
 		this.preco = preco;
@@ -13,7 +13,21 @@ public class Produto {
 	public Produto() {
 		super();
 	}
-	
-	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
 
 }
